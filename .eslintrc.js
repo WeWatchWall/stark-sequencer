6141f2f959f9ac392571cfa84f042969fabbcb5c
Adrian Burlacu <adrian.burlacu@live.com>
2021-08-27 15:54:10 +0300

    Try linting without :P

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,7 @@ module.exports = {
   'extends': [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/essential'
   ],
 
   parserOptions: {
